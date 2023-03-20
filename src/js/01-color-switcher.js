2561565dffe30f2b98a0fbaadfb5bf7a0d3132a4
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-20 11:19:33 +0200

    #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
 
+};
+
+
+refs.btnStart.addEventListener('click', onStartChangeBgColor);
+refs.btnStop.addEventListener('click', onStopChangeBgColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  let timer = null;
+
+  function onStartChangeBgColor() {
+    timer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStart.disabled = true;
+  }
+
+  function onStopChangeBgColor() {
+    clearInterval(timer);
+    refs.btnStart.disabled = false;
+  }
