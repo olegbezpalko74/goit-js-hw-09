207649bb8c8b06088128f966afc96acad1227b6c
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-20 22:57:55 +0200

    #3 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -29,11 +29,11 @@ function onSubmitForm(e) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
 
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
     delay += step;
   }
